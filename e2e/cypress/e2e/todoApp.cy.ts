1da69640bc10b8f3d3922f861201658f167b10b6
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2024-05-28 10:52:30 +0200

    ✨ Mocking cypress fetch

--- e2e/cypress/e2e/todoApp.cy.ts
+++ e2e/cypress/e2e/todoApp.cy.ts
@@ -1,3 +1,4 @@
+import { Todo } from "./../../src/models/Todo";
 describe("Todo app", () => {
   beforeEach(() => {
     cy.visit("/");
@@ -5,6 +6,9 @@ describe("Todo app", () => {
 
   it("should add a todo", () => {
     // Assign
+    cy.intercept("POST", "http://awesomeurl.com/create/*", {
+      statusCode: 201,
+    });
 
     // Act
     cy.get("input#todoText").type("Testing, testing");
@@ -14,6 +18,20 @@ describe("Todo app", () => {
     cy.get("ul > li")
       .should("have.length", 1)
       .should("have.text", "Testing, testing");
+  });
+
+  it("should not add a todo due to error", () => {
+    // Assign
+    cy.intercept("POST", "http://awesomeurl.com/create/*", {
+      statusCode: 418,
+    });
+
+    // Act
+    cy.get("input#todoText").type("Testing, testing");
+    cy.get("button#save").click();
+
+    // Assert
+    cy.get("ul > li").should("have.length", 0);
   });
 
   it("should not add empty todo", () => {
@@ -28,6 +46,10 @@ describe("Todo app", () => {
 
   it("should toggle the todo", () => {
     // Assign
+    cy.intercept("POST", "http://awesomeurl.com/create/*", {
+      statusCode: 201,
+    });
+
     cy.get("input#todoText").type("Testing, testing");
     cy.get("button#save").click();
     cy.get("ul > li")
@@ -40,5 +62,29 @@ describe("Todo app", () => {
 
     // Assert
     cy.get("ul > li:first").should("have.class", "done");
+  });
+
+  it("should get todos from api", () => {
+    cy.intercept("http://awesomeurl.com*", {
+      todos: [new Todo("Test 1"), new Todo("Test 2")],
+    });
+
+    cy.get("button#getTodos").click();
+
+    cy.get("ul > li").should("have.length", 2);
+    cy.get("ul > li:first").should("have.text", "Test 1");
+  });
+
+  it("should make the request with correct value", () => {
+    cy.intercept("POST", "http://awesomeurl.com/create/*", {
+      statusCode: 201,
+    }).as("saveTodo");
+
+    // Warning: This will be used in a url. Do not use strange characters :)
+    const userInput = "Testing";
+    cy.get("input#todoText").type(userInput);
+    cy.get("button#save").click();
+
+    cy.wait("@saveTodo").its("request.url").should("contain", userInput);
   });
 });
--- e2e/src/main.ts
+++ e2e/src/main.ts
@@ -1,31 +1,49 @@
+import { IApiResponse } from "./models/IApiResponse";
 import { Todo } from "./models/Todo";
 import "./style.css";
 
 const todos: Todo[] = [];
 
-document.getElementById("save")?.addEventListener("click", () => {
+document.getElementById("save")?.addEventListener("click", async () => {
   const userInput = (document.getElementById("todoText") as HTMLInputElement)
     .value;
 
   if (userInput !== "") {
-    todos.push(new Todo(userInput));
-    createHtml(todos);
+    const response = await fetch(`http://awesomeurl.com/create/${userInput}`, {
+      method: "POST",
+    });
+
+    if (response.status === 201) {
+      todos.push(new Todo(userInput));
+      createHtml(todos);
+    } else {
+      //TODO: Present error message to user
+    }
   }
 });
 
-const createHtml = (todos: Todo[]) => {
+document.getElementById("getTodos")?.addEventListener("click", async () => {
+  const response = await fetch("http://awesomeurl.com/");
+  const data: IApiResponse = await response.json();
+
+  //TODO: Lägg in datan i variabeln todos
+
+  createHtml(data.todos);
+});
+
+const createHtml = (theTodos: Todo[]) => {
   const todoList = document.getElementById("todos") as HTMLUListElement;
 
   todoList.innerHTML = "";
 
-  for (let i = 0; i < todos.length; i++) {
+  for (let i = 0; i < theTodos.length; i++) {
     const liTag = document.createElement("li");
-    liTag.innerHTML = todos[i].text;
-    liTag.className = todos[i].done ? "done" : "";
+    liTag.innerHTML = theTodos[i].text;
+    liTag.className = theTodos[i].done ? "done" : "";
 
     liTag.addEventListener("click", () => {
-      todos[i].done = !todos[i].done;
-      createHtml(todos);
+      theTodos[i].done = !theTodos[i].done;
+      createHtml(theTodos);
     });
 
     todoList.appendChild(liTag);
